2ae54e357a9459398ec3c40911bc3c094f099f70
LEWISpatrick <133691910+LEWISpatrick@users.noreply.github.com>
2024-05-11 15:43:06 -0300

    fixed fronted api key!

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider frontendApi="pk_test_cHJlc2VudC1tYXlmbHktMjYuY2xlcmsuYWNjb3VudHMuZGV2JA">
 
        <html lang="en">
       <body className={inter.className}>
